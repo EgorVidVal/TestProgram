936a51f803982b65271f373c55facf92b6a3c1f6
Apokadr@outlook.com <Apokadr@outlook.com>
2019-12-16 15:15:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test t = new test();
+
+            t.gname = 2;
+
+            Console.WriteLine(t.dachka());
+            t.gname = 4;
+
+            Console.WriteLine(t.dachka());
+
+            Console.ReadKey();
+
+            t.nammme = "привет";
+            Console.WriteLine(t.nammme);
+
+        }
+    }
+}
---
+++ ConsoleApp6/test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class test
+    {
+        private int qq;
+   
+        public int gname
+        {
+            get { return qq; }
+            set { qq = value; }
+        }
+
+        public int dachka()
+        {
+            int z = qq * 2;
+            return z;
+        }
+
+        public string nammme { get; set; }
+    }
+}
